5da271d24ac7882de25cb4be736dcc1c445337b4
dergaa abdelmalek <GAMER.STORY007@GMAIL.COM>
2024-12-29 15:43:59 +0100

    added auth

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,8 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -18,5 +20,10 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+        User::create([
+            'name'=>'melek',
+            'email' => 'melekdergaaofficial@gmail.com',  
+            'password' => Hash::make('melekdergaa'),  
+        ]);
     }
 }
